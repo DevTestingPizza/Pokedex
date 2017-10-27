d5d63b25797a2f6a1ea1e0939da3366aa6cabf2c
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-27 12:40:26 +0200

    Main UI is done

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -13,11 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+//        print(charmander.name, charmander.pokedexId)
+        
     }
 
 

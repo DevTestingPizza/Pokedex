f118a19553e3287f1d78e3ad37eb4619d4f7789f
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-27 15:09:50 +0200

    Create new PokemonDetailVC (viewcontroller) for detailed pokemon info

---
+++ Pokedex/Controller/PokemonDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Tom Grobbe on 27/10/2017.
+//  Copyright © 2017 Tom Grobbe. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}

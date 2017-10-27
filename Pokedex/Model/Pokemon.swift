2fb287ac591e97a3db1f7dcc1b1db2a304f8432f
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-27 12:12:21 +0200

    Created pokemon class

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Tom Grobbe on 27/10/2017.
+//  Copyright © 2017 Tom Grobbe. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+    
+}
+

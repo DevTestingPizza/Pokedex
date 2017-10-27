bf5f7b13ee4e2cd16c01a3f1aeaed687bf3cd128
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-27 12:51:33 +0200

    Created PokeCell class and connected @IBOutlets to the thumbnail image and label

---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Tom Grobbe on 27/10/2017.
+//  Copyright © 2017 Tom Grobbe. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}

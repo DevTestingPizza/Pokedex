fcf1f5ae7e067c329842ee69c717a8a710e22f59
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-27 13:08:12 +0200

    Implemented protocols and required functions, also rounded the corners of each PokeCell

--- Pokedex/View/PokeCell.swift
+++ Pokedex/View/PokeCell.swift
@@ -14,11 +14,19 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
         
         nameLabel.text = self.pokemon.name.capitalized
         thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
     }
     
 }
